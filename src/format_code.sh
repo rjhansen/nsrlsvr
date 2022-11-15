99f5f301b87b2626c454ab6b37b8015ccff5f9a6
Robert J. Hansen <rob@hansen.engineering>
2022-11-15 13:04:58 -0500

    Cleaning workspace for Rust.

--- src/format_code.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-for x in *.cc ; do clang-format -style=Mozilla $x > tidycode ; mv tidycode $x ; done
-for x in *.h ; do clang-format -style=Mozilla $x > tidycode ; mv tidycode $x ; done
